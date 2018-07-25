67215b8093a2871ab89da817d98b09cb0af25219
tsrnc2 <40697009+tsrnc2@users.noreply.github.com>
2018-07-25 08:45:34 -0700

    Update cmf.spec.ts
    remove place card holder 'f' and un comment test

--- src/strategy/indicators/cmf.spec.ts
+++ src/strategy/indicators/cmf.spec.ts
@@ -1,9 +1,9 @@
 import { CMF } from './cmf'
 import { candles } from './spec.helper'
 
-fdescribe('CMF', () => {
-  fit('should calculate the correct cmf', () => {
+describe('CMF', () => {
+  it('should calculate the correct cmf', () => {
     const cmf = CMF.calculate(candles.slice(0, 19), 20)
- //   expect(cmf).toEqual(1.099868227)
+     expect(cmf).toEqual(1.099868227)
   })
 })

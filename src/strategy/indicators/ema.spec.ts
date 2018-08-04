0f8c14ac67fa0985288e79666f9e70cba1628780
Shawn8901 <shawn8901@googlemail.com>
2018-08-04 20:22:50 +0200

    Added test cases for 2 branches which were missing in the tests

--- src/strategy/indicators/ema.spec.ts
+++ src/strategy/indicators/ema.spec.ts
@@ -3,9 +3,21 @@ import { EMA } from './ema'
 import { candles } from './spec.helper'
 
 describe('EMA', () => {
+
+  it('should return null, if not enough data', () => {
+    expect(EMA.calculate(0, candles.slice(0, 1), 15, 'close')).toBeNull()
+  })
+
+  it('should calculate the sma for first ema period', () => {
+    const sma = SMA.calculate(candles.slice(1, 16), 14)
+    const ema = EMA.calculate(0, candles.slice(1, 16), 14)
+    expect(ema).toEqual(sma)
+  })
+
   it('should calculate the correct ema', () => {
     const sma = SMA.calculate(candles.slice(1, 15), 14)
     const ema = EMA.calculate(sma, candles.slice(0, 15), 14)
     expect(ema).toEqual(17.533047619047622)
   })
+
 })

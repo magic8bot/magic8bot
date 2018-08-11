402fff01af6c24292537cd1de5d6d2b691b0cf35
Vitaliy Isikov <visikov@gmail.com>
2018-08-10 23:34:46 -0400

    Get RSI spec to :100:

--- src/strategy/indicators/rsi.spec.ts
+++ src/strategy/indicators/rsi.spec.ts
@@ -2,19 +2,34 @@ import { RSI } from './rsi'
 import { candles } from './spec.helper'
 
 describe('RSI', () => {
-  it('should calculate first averages correctly', () => {
+  test('calculates the first averages correctly', () => {
     const { avgGain, avgLoss, rsi } = RSI.calculate(0, 0, candles.slice(1, 16), 14)
     expect(avgGain).toEqual(0.267142857142857)
     expect(avgLoss).toEqual(0.1957142857142856)
     expect(rsi).toEqual(57.72)
   })
 
-  it('should calculate rsi correctly', () => {
+  test('calculates rsi correctly', () => {
     const { avgGain: ag, avgLoss: al } = RSI.calculate(0, 0, candles.slice(1, 16), 14)
 
     const { avgGain, avgLoss, rsi } = RSI.calculate(ag, al, candles.slice(0, 16), 14)
     expect(avgGain).toEqual(0.3116326530612244)
     expect(avgLoss).toEqual(0.1817346938775509)
     expect(rsi).toEqual(63.16)
+  })
+
+  test('calculates rsi when avgLoss === 0', () => {
+    jest.spyOn<any, any>(RSI, 'getAvgGainLoss').mockReturnValueOnce({ avgGain: 0, avgLoss: 0 })
+
+    const { avgGain, avgLoss, rsi } = RSI.calculate(0, 0, candles.slice(1, 16), 14)
+    expect(avgGain).toEqual(0)
+    expect(avgLoss).toEqual(0)
+    expect(rsi).toEqual(100)
+  })
+
+  test('calculates avgGain when currentGain === 0', () => {
+    const { avgGain, avgLoss } = RSI.getAvgGainLoss(1, 1, [{ close: 1 }, { close: 2 }] as any, 1)
+    expect(avgGain).toEqual(0)
+    expect(avgLoss).toEqual(1)
   })
 })

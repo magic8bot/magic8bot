07860b54fef9144ea53244af7e088094c62919d5
Vitaliy Isikov <3137491+notVitaliy@users.noreply.github.com>
2018-08-12 17:30:09 -0400

    Disable CCXT ratelimiter/Add Bottleneck (#220)
    * Disable CCXT ratelimiter/Add Bottleneck
    
    * Remove redundant return typings
    
    * Missed one
    
    * Remove unused imports

--- src/exchange/adapters/base.ts
+++ src/exchange/adapters/base.ts
@@ -2,6 +2,7 @@ import { Trade } from 'ccxt'
 
 export interface ExchangeAdapter {
   scan: 'back' | 'forward'
+  ratelimit: number
   getTradeCursor: (trade: Trade) => number
   mapTradeParams: (start: number) => { [key: string]: number }
 }

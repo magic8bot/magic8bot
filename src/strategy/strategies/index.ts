416b8092733e8d4dad87eb50fb73ae030c9dbeb4
Shawn8901 <shawn8901@googlemail.com>
2018-08-13 23:53:56 +0200

    Refactored strategyloader to be able to load dynamic modules from the strategy folder. A strategy has to export the strategy itself as variable strategy to the module context.

--- src/strategy/strategies/index.ts
+++ src/strategy/strategies/index.ts
@@ -1,12 +1,14 @@
-import { Macd } from './macd'
-
+import { logger } from '@util'
 export const strategyLoader = (strategyName: string) => {
-  switch (strategyName) {
-    case 'macd':
-      return Macd
-
-    default:
-      throw new Error(`${strategyName} not implemented yet.`)
+  try {
+    if (strategyName === null || strategyName === undefined || !strategyName.length) throw new Error('No strategy given.')
+    logger.verbose(`Loading ${strategyName}....`)
+    const strategyModule = require(`./${strategyName}`)
+    if (!strategyModule.strategy) throw Error("Wrong exported strategy. Export Strategy as field 'strategy'.")
+    logger.silly(`Loaded ${strategyName} successfully.`)
+    return strategyModule.strategy
+  } catch (e) {
+    throw new Error(`Strategy-Module ${strategyName} has thrown an error when loaded! Error-Message: ${e}`)
   }
 }
 
--- src/strategy/strategies/macd/index.ts
+++ src/strategy/strategies/macd/index.ts
@@ -1,2 +1,5 @@
-export * from './macd'
-export * from './phenotypes'
+import { MacdPhenotypes } from './phenotypes'
+import { Macd } from './macd'
+
+export const strategy = Macd
+export const phenotypes = MacdPhenotypes

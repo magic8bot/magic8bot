4c482fc84ab8897b272760b01a7c64a28f71e49a
Vitaliy Isikov <visikov@gmail.com>
2018-08-17 00:10:27 -0400

    Start working on ChaosExchange

---
+++ src/exchange/adapters/chaos.ts
@@ -0,0 +1,18 @@
+import { Trade } from 'ccxt'
+import { ExchangeAdapter } from './base'
+import { time } from '@util'
+
+export const binance: ExchangeAdapter = {
+  scan: 'forward',
+  ratelimit: 50,
+
+  mapTradeParams: (startTime: number) => {
+    if (startTime === null) return null
+    const endTime = time(startTime).add.h(1)
+    return { startTime, endTime }
+  },
+
+  getTradeCursor: (trade: Trade) => {
+    return trade.timestamp
+  },
+}

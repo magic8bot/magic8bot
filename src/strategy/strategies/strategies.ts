1920408829db54f027edbef657d43327b35c23fb
tsrnc2 <40697009+tsrnc2@users.noreply.github.com>
2018-09-08 16:41:51 -0700

    reset macd as default

--- src/strategy/strategies/strategies.ts
+++ src/strategy/strategies/strategies.ts
@@ -1,4 +1,4 @@
 import { strategy as macd } from './macd'
 import { strategy as softcodedprice } from './softcodedprice'
 
-export const Strategies = { softcodedprice }
+export const Strategies = { macd }
